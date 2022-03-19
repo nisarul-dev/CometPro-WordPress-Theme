6f1870a70f19d7a4fc38e51b5114c73a9e324c02
Nisarul Amin Naim <65971192+nisarul-media@users.noreply.github.com>
2022-03-19 23:10:51 +0600

    Including the Styles

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
  */
 
 // Theme setup functions
-function commetpro_setup_theme() {
+function cometpro_setup_theme() {
     // Text Domain
     load_theme_textdomain( 'cometpro', get_template_directory() . '/lang' );
 
@@ -18,9 +18,14 @@ function commetpro_setup_theme() {
     add_theme_support( 'post-thumbnail' );
     add_theme_support( 'title-tag' );
 }
-add_action( 'after_setup_theme', 'commetpro_setup_theme' );
+add_action( 'after_setup_theme', 'cometpro_setup_theme' );
 
-
-
+// Including Stylesheets
+function cometpro_styles_enqueue() {
+    wp_enqueue_style( 'cometpro-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'bundle', get_template_directory_uri() . '/css/bundle.css', [], '3.3.5', 'all' );
+    wp_enqueue_style( 'custom-style', get_template_directory_uri() . '/css/style.css', [], '1.0.0', 'all' );
+}
+add_action( 'wp_enqueue_scripts', 'cometpro_styles_enqueue' );
 
 
