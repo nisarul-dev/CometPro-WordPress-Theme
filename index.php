4d53db427695f66639f8a8250cf8e262834177fa
Nisarul Amin Naim <65971192+nisarul-media@users.noreply.github.com>
2022-03-22 13:27:10 +0600

    Update: Standard Post Format

--- index.php
+++ index.php
@@ -242,6 +242,16 @@
       <div class="container">
         <div class="col-md-8">
 
+          <?php
+            if( have_posts() ) :
+              while( have_posts() ) :
+                the_post();
+                echo get_post_format();
+                get_template_part( 'template-parts/content', get_post_format() );
+              endwhile;
+            endif;
+          ?>
+
           <!-- Posts -->
           <div class="blog-posts">
 
---
+++ template-parts/content.php
@@ -0,0 +1,11 @@
+            <article class="post-single">
+              <div class="post-info">
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <h6 class="upper"><span>By</span><a href="<?php echo esc_url( get_the_author_meta( 'url' ) ) ?>"  title="<?php echo esc_attr( sprintf( __( 'Visit %s&#8217;s website' ), get_the_author() ) ); ?>" rel="author external" target="_blank"> <?php the_author(); ?></a><span class="dot"></span><span><?php the_time('j F Y'); ?></span><span class="dot"></span><?php the_tags(); ?></h6>
+              </div>
+              <div class="post-media"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a></div>
+              <div class="post-body">
+                <?php echo wp_trim_words( get_the_content(), 50, '' ); ?>
+                <p><a href="<?php the_permalink(); ?>" class="btn btn-color btn-sm">Read More</a></p>
+              </div>
+            </article>
