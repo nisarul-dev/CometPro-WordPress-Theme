60b7f01f4a7492fcc4bc5cab3600766e34c1fdd1
Nisarul Amin Naim <65971192+nisarul-media@users.noreply.github.com>
2022-03-28 14:34:26 +0600

    Update content.php, content-quote.php and index.php

--- index.php
+++ index.php
@@ -242,99 +242,18 @@
       <div class="container">
         <div class="col-md-8">
 
-          <?php
-            if( have_posts() ) :
-              while( have_posts() ) :
-                the_post();
-                echo get_post_format();
-                get_template_part( 'template-parts/content', get_post_format() );
-              endwhile;
-            endif;
-          ?>
+
 
           <!-- Posts -->
           <div class="blog-posts">
-
-            <!-- Gallery Post -->
-            <article class="post-single">
-              <div class="post-info">
-                <h2><a href="#">Checklists for Startups</a></h2>
-                <h6 class="upper"><span>By</span><a href="#"> Admin</a><span class="dot"></span><span>28 September 2015</span><span class="dot"></span><a href="#" class="post-tag">Startups</a></h6>
-              </div>
-              <div class="post-media">
-                <div data-options="{&quot;animation&quot;: &quot;slide&quot;, &quot;controlNav&quot;: true" class="flexslider nav-outside">
-                  <ul class="slides">
-                    <li><img src="<?php echo get_template_directory_uri() . '/images' ?>/blog/1.jpg" alt=""></li>
-                    <li><img src="<?php echo get_template_directory_uri() . '/images' ?>/blog/2.jpg" alt=""></li>
-                    <li><img src="<?php echo get_template_directory_uri() . '/images' ?>/blog/3.jpg" alt=""></li>
-                  </ul>
-                </div>
-              </div>
-              <div class="post-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae ut ratione similique temporibus tempora dicta soluta? Qui hic, voluptatem nemo quo corporis dignissimos voluptatum debitis cumque fugiat mollitia quasi quod. Repudiandae possimus quas odio nisi optio asperiores, vitae error laudantium, ratione odit ipsa obcaecati debitis deleniti minus, illo maiores placeat omnis magnam.</p>
-                <p><a href="#" class="btn btn-color btn-sm">Read More</a></p>
-              </div>
-            </article>
-
-            <!-- Video Post -->
-            <article class="post-single">
-              <div class="post-info">
-                <h2><a href="#">Never Tell People What You Do</a></h2>
-                <h6 class="upper"><span>By</span><a href="#"> Admin</a><span class="dot"></span><span>28 September 2015</span><span class="dot"></span><a href="#" class="post-tag">Entrepreneurship</a></h6>
-              </div>
-              <div class="post-media">
-                <div class="media-video">
-                  <iframe src="https://www.youtube.com/embed/rrT6v5sOwJg" frameborder="0"></iframe>
-                </div>
-              </div>
-              <div class="post-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae ut ratione similique temporibus tempora dicta soluta? Qui hic, voluptatem nemo quo corporis dignissimos voluptatum debitis cumque fugiat mollitia quasi quod. Repudiandae possimus quas odio nisi optio asperiores, vitae error laudantium, ratione odit ipsa obcaecati debitis deleniti minus, illo maiores placeat omnis magnam.</p>
-                <p><a href="#" class="btn btn-color btn-sm">Read More</a></p>
-              </div>
-            </article>
-
-            <!-- Quote Post -->
-            <article class="post-single">
-              <div class="post-info">
-                <h2><a href="#">Give It Five Minutes</a></h2>
-                <h6 class="upper"><span>By</span><a href="#"> Admin</a><span class="dot"></span><span>28 September 2015</span><span class="dot"></span><a href="#" class="post-tag">Startups</a></h6>
-              </div>
-              <div class="post-body">
-                <blockquote class="italic">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et quam neque facilis similique laborum, nihil id ratione, error illum. Porro quas maxime accusamus numquam consequatur consequuntur eveniet quis, fuga repellendus.</p>
-                </blockquote>
-              </div>
-            </article>
-
-            <!-- Standard Post -->
-            <article class="post-single">
-              <div class="post-info">
-                <h2><a href="#">Uber Launches in Las Vegas</a></h2>
-                <h6 class="upper"><span>By</span><a href="#"> Admin</a><span class="dot"></span><span>28 September 2015</span><span class="dot"></span><a href="#" class="post-tag">Startups</a></h6>
-              </div>
-              <div class="post-media"><a href="#"><img src="<?php echo get_template_directory_uri() . '/images' ?>/blog/4.jpg" alt=""></a></div>
-              <div class="post-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae ut ratione similique temporibus tempora dicta soluta? Qui hic, voluptatem nemo quo corporis dignissimos voluptatum debitis cumque fugiat mollitia quasi quod. Repudiandae possimus quas odio nisi optio asperiores, vitae error laudantium, ratione odit ipsa obcaecati debitis deleniti minus, illo maiores placeat omnis magnam.</p>
-                <p><a href="#" class="btn btn-color btn-sm">Read More</a></p>
-              </div>
-            </article>
-
-            <!-- Audio Post -->
-            <article class="post-single">
-              <div class="post-info">
-                <h2><a href="#">Fun With Product Hunt</a></h2>
-                <h6 class="upper"><span>By</span><a href="#"> Admin</a><span class="dot"></span><span>28 September 2015</span><span class="dot"></span><a href="#" class="post-tag">Tech</a></h6>
-              </div>
-              <div class="post-media">
-                <div class="media-audio">
-                  <iframe src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/51057943&amp;amp;color=ff5500&amp;amp;auto_play=false&amp;amp;hide_related=false&amp;amp;show_comments=true&amp;amp;show_user=true&amp;amp;show_reposts=false" frameborder="0"></iframe>
-                </div>
-              </div>
-              <div class="post-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae ut ratione similique temporibus tempora dicta soluta? Qui hic, voluptatem nemo quo corporis dignissimos voluptatum debitis cumque fugiat mollitia quasi quod. Repudiandae possimus quas odio nisi optio asperiores, vitae error laudantium, ratione odit ipsa obcaecati debitis deleniti minus, illo maiores placeat omnis magnam.</p>
-                <p><a href="#" class="btn btn-color btn-sm">Read More</a></p>
-              </div>
-            </article>
+            <?php
+              if( have_posts() ) :
+                while( have_posts() ) :
+                  the_post();
+                  get_template_part( 'template-parts/content', get_post_format() );
+                endwhile;
+              endif;
+            ?>
           </div>
           <ul class="pagination">
             <li><a href="#" aria-label="Previous"><span aria-hidden="true"><i class="ti-arrow-left"></i></span></a></li>
--- template-parts/content-quote.php
+++ template-parts/content-quote.php
@@ -1,7 +1,7 @@
             <article class="post-single">
               <div class="post-info">
                 <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <h6 class="upper"><span>By</span><a href="<?php echo esc_url( get_the_author_meta( 'url' ) ) ?>"  title="<?php echo esc_attr( sprintf( __( 'Visit %s&#8217;s website' ), get_the_author() ) ); ?>" rel="author external" target="_blank"> <?php the_author(); ?></a><span class="dot"></span><span><?php the_time('j F Y'); ?></span><?php echo ( '' != the_tags() ) ? '<span class="dot"></span>' . the_tags() : ''; ?></h6>
+                <h6 class="upper"><span>By</span><a href="<?php echo esc_url( get_the_author_meta( 'url' ) ) ?>"  title="<?php echo esc_attr( sprintf( __( 'Visit %s&#8217;s website' ), get_the_author() ) ); ?>" rel="author external" target="_blank"> <?php the_author(); ?></a><span class="dot"></span><span><?php the_time('j F Y'); ?></span><?php echo ( '' != get_the_tags() ) ? '<span class="dot"></span>' . get_the_tag_list( null, ', ', '' ) : ''; ?></h6>
               </div>
               <div class="post-body">
                 <blockquote class="italic">
