73dcdd0aa7313b14760c30988d74c4d8335eeddb
Nisarul Amin Naim <65971192+nisarul-media@users.noreply.github.com>
2022-04-10 02:27:49 +0600

    JS code refactor

--- js/admin-dashboard.js
+++ js/admin-dashboard.js
@@ -1,79 +1,80 @@
 (function($){
   'use strict';
 
-  // The hiding MataBox Game function
-  function hidingGame() {
-    if ($(".editor-post-format #post-format-selector-0")) { // Gutenberg
-      $(".editor-post-format #post-format-selector-0").change(hidingMetboxes);
-    }
-    if ($('input[type=radio][name=post_format]')) { // Classic Editor
-      $('input[type=radio][name=post_format]').change( hidingMetboxes );
-    }
-    function hidingMetboxes() {
-      var currentFormat = $(this).val();
+  // The hiding MataBox Game function with 4 parameters
+  function hidingGame( currentFormat, intervalToBeCleared = undefined, func = undefined ) {
+    $( '#cometpro_demo_metabox' ).show(); // MetaBox Container(Parent)-> Show
+    $( '.cmb-row' ).hide(); // Fields(Childrens)-> Hide
 
-      if (currentFormat == "0") { // For Classic Editor
-        currentFormat = "standard";
-      };
+    switch ( currentFormat ) {
+      case 'standard':
+        $( '#cometpro_demo_metabox' ).hide(); // MetaBox Container-> Hide
+        break;
 
-      if (currentFormat == 'standard' || currentFormat == 'quote') {
-        $('.cmb-row').hide();
-        $('#cometpro_demo_metabox').hide();
-      }
-      if (currentFormat == 'audio') {
-        $('#cometpro_demo_metabox').show();
-        $('.cmb-row').hide();
-        $('.cmb-row.cmb2-id-cometpro-soundcloud-embed').show();
-      }
-      if (currentFormat == 'video') {
-        $('#cometpro_demo_metabox').show();
-        $('.cmb-row').hide();
-        $('.cmb-row.cmb2-id-cometpro-youtube-embed').show();
-      }
-      if (currentFormat == 'gallery') {
-        $('#cometpro_demo_metabox').show();
-        $('.cmb-row').hide();
-        $('.cmb-row.cmb2-id-cometpro-gallery-embed').show();
-      }
+      case 'quote':
+        $( '#cometpro_demo_metabox' ).hide(); // MetaBox Container-> Hide
+        break;
+
+      case 'audio':
+        $( '.cmb-row.cmb2-id-cometpro-soundcloud-embed' ).show(); // Audio Field-> Show
+        break;
+
+      case 'video':
+        $( '.cmb-row.cmb2-id-cometpro-youtube-embed' ).show(); // Video Field-> Show
+        break;
+
+      case 'gallery':
+        $( '.cmb-row.cmb2-id-cometpro-gallery-embed' ).show(); // Gallery Field-> Show
+        break;
+
+      default:
+        $( '.cmb-row' ).show();
+        break;
+    }
+
+    if ( intervalToBeCleared ) { // If the interval argument is passed, then clear it.
+      clearInterval( intervalToBeCleared );
+    }
+
+    if ( func ) { // If a function is passed as an argument, then cll the function.
+      func();
     }
   }
 
-  // Game starts from here
-  var the_interval = setInterval( function () {
-    var currentType = $(".editor-post-format #post-format-selector-0").val();
+  function metaBoxEventLisners() {
+    if ($( '.editor-post-format #post-format-selector-0' )) { // Gutenberg - Select Option (jQuery)
+      $( '.editor-post-format #post-format-selector-0' ).change(hidingMetboxes);
+    }
+    if ($( 'input[type=radio][name=post_format]')) { // Classic Editor - Radio Input (JS DOM)
+      document.querySelector( '#post-formats-select').addEventListener( "change", hidingMetboxes );
+    }
+    function hidingMetboxes( e ) {
+      var currentFormat = e.target.value;
 
-    if( !currentType ) { // For Classic Editor
-      var currentType = $('input[type=radio][name=post_format]:checked')[0].getAttribute('value');
-      if (currentType == "0") {
-        currentType = "standard";
+      if ( currentFormat == "0" ) { // For Classic Editor
+        currentFormat = "standard";
       };
-    }
 
-    // Initial hiding Metaboxs
-    if (currentType == "standard" || currentType == "quote") {
-      $('.cmb-row').hide();
-      $('#cometpro_demo_metabox').hide();
-      hidingGame();
-      clearInterval( the_interval );
+      hidingGame( currentFormat );
+
     }
-    if (currentType == "audio") {
-      $('.cmb-row').hide();
-      $('.cmb-row.cmb2-id-cometpro-soundcloud-embed').show();
-      hidingGame();
-      clearInterval( the_interval );
-    }
-    if( currentType == "video" ) {
-      $('.cmb-row').hide();
-      $('.cmb-row.cmb2-id-cometpro-youtube-embed').show();
-      hidingGame();
-      clearInterval( the_interval );
-    }
-    if( currentType == "gallery" ) {
-      $('.cmb-row').hide();
-      $('.cmb-row.cmb2-id-cometpro-gallery-embed').show();
-      hidingGame();
-      clearInterval( the_interval );
-    }
-  }, 1000);
+  }
+
+  // Custom MetaBox Hiding Game starts from here
+  if ( $( '#poststuff' ) ) {
+    var metaBoxInterval = setInterval( function () {
+      var currentType = $( '.editor-post-format #post-format-selector-0' ).val();
+
+      if( !currentType ) { // For Classic Editor
+        var currentType = $( 'input[type=radio][name=post_format]:checked' )[0].getAttribute( 'value' );
+        if ( currentType == "0" ) {
+          currentType = "standard";
+        }
+      }
+
+      hidingGame( currentType, metaBoxInterval, metaBoxEventLisners);
+
+    }, 1000);
+  }
 
 })(jQuery)
